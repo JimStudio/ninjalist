2cc59bfe6dd51ea0cd30440970653ad744f98b0e
JimStudio <chuixiaojian@hotmail.com>
2023-01-06 03:44:53 +0800

    new

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -1,3 +1,18 @@
+export const getStaticPaths = async() => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
+
+    const paths = data.map(ninja => {
+        return{
+            parmas: {id:ninja.id.toString()}
+        }
+    })
+    return {
+        paths,
+        fallback: false;
+    }
+}
+
 const Detalis = () => {
     return ( 
         <div>Datelis Page</div>
