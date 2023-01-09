fe62798e61be5f14e3cc0f7d6387f9672f8cff2b
Your Name <you@example.com>
2023-01-09 20:03:49 +0800

    initia

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -9,17 +9,19 @@ export const getStaticPaths = async() => {
             params: { id: ninja.id.toString() }
         }
     })
+   
+
     return {
         paths,
         fallback: false
     }
+
 }
 //获取指定的数据,并显示获取到的数据
 export const getStaticProps = async (context) => {
     const id = context.params.id;
     const res = await fetch('https://jsonplaceholder.typicode.com/users/' + id);
    const data = await res.json();
-
    return{
      props: { ninja: data}
    }
